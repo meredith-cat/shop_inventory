95956b4b9ada8d96a34733f7ea1ce641711d07b0
Meredith Gallop <meredith@trtl.co.uk>
2020-02-03 11:16:13 +0000

    edit potion fixed

--- controllers/potions_controller.rb
+++ controllers/potions_controller.rb
@@ -23,17 +23,23 @@ get '/potions/:id' do
   erb(:'potions/show')
 end
 
-post '/potions/:id/delete' do
-  @potion = Potion.find(params[:id])
-  @potion.delete
-  redirect to ('/potions')
-end
-
 get '/potions/:id/edit' do
   @potion = Potion.find(params[:id])
   @makers = Maker.all
   @types = Type.all
   erb(:'potions/edit')
+end
+
+post '/potions/:id' do
+  potion = Potion.new(params)
+  potion.update
+  redirect to "/potions"
+end
+
+post '/potions/:id/delete' do
+  @potion = Potion.find(params[:id])
+  @potion.delete
+  redirect to ('/potions')
 end
 
 post '/potions' do
