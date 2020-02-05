b851ebde14d1aebf968d27f16b704980c803f371
Meredith Gallop <meredith@trtl.co.uk>
2020-02-05 10:34:08 +0000

    consistent buttons over site

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ potion3 = Potion.new({'name' => 'Drink Me', 'description' => 'Makes you bigger..
 potion3.save
 potion4 = Potion.new({'name' => 'Chicken Soup', 'description' => 'It\'s just some chicken soup.', 'quantity' => 2, 'cost_price' => 1, 'sale_price' => 3, 'maker_id' => maker1.id, 'type_id' => type1.id})
 potion4.save
-potion5 = Potion.new({'name' => 'Flying Ointment', 'description' => 'You\'ll fly! (Probably)', 'quantity' => 6, 'cost_price' => 18, 'sale_price' => 21, 'maker_id' => maker4.id, 'type_id' => type1.id})
+potion5 = Potion.new({'name' => 'Flying Ointment', 'description' => 'You\'ll fly! (Probably)', 'quantity' => 6, 'cost_price' => 18, 'sale_price' => 21, 'maker_id' => maker4.id, 'type_id' => type4.id})
 potion5.save
 potion6 = Potion.new({'name' => 'Polyjuice', 'description' => 'Add some hair or a toenail, and transform!', 'quantity' => 2, 'cost_price' => 80, 'sale_price' => 83, 'maker_id' => maker5.id, 'type_id' => type3.id})
 potion6.save
@@ -44,6 +44,12 @@ potion7 = Potion.new({'name' => 'Draught Of Sleep', 'description' => 'Zzzzz.....
 potion7.save
 potion8 = Potion.new({'name' => 'XXX', 'description' => 'Probably poison.', 'quantity' => 0, 'cost_price' => 47, 'sale_price' => 66, 'maker_id' => maker3.id, 'type_id' => type2.id})
 potion8.save
+potion9 = Potion.new({'name' => 'Ent-Draught', 'description' => 'Brings refreshment and vigour.', 'quantity' => 4, 'cost_price' => 15, 'sale_price' => 50, 'maker_id' => maker2.id, 'type_id' => type1.id})
+potion9.save
+potion10 = Potion.new({'name' => 'Ambrosia', 'description' => 'Drink of the gods, or rice pudding?', 'quantity' => 0, 'cost_price' => 0, 'sale_price' => 1000, 'maker_id' => maker1.id, 'type_id' => type4.id})
+potion10.save
+potion11 = Potion.new({'name' => 'Warming Mead', 'description' => 'Delicious spiced mead which warms from within.', 'quantity' => 8, 'cost_price' => 12, 'sale_price' => 25, 'maker_id' => maker2.id, 'type_id' => type1.id})
+potion11.save
 
 binding.pry
 nil
